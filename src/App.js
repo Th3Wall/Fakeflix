be57f656f64cc0cb9073805adc8dc05648ae2565
Th3Wall <mandellidavide.95@gmail.com>
2021-02-24 00:15:16 +0100

    Project setup with prettier, eslint, husky & lint-staged

--- src/App.js
+++ src/App.js
@@ -1,25 +1,11 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.scss"
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            <h1>Welcome to Fakeflix</h1>
+        </div>
+    )
 }
 
-export default App;
+export default App
