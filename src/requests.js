80bbb3ab76a3c392c4cc31234955351cec168735
Th3Wall <mandellidavide.95@gmail.com>
2021-02-24 02:41:46 +0100

    Quick fix to toprated url

--- src/requests.js
+++ src/requests.js
@@ -3,7 +3,7 @@ const API_KEY = 'e8d53ad78d99a4722c3f0f0f6a5c9014';
 const requests = {
     fetchTrending: `/trending/all/week?api_key=${API_KEY}&language=en-US`,
     fetchPopular: `/movie/popular?api_key=${API_KEY}&language=en-US`,
-    fetchTopRated: `movie/top_rated?api_key=${API_KEY}&language=en-US`
+    fetchTopRated: `/movie/top_rated?api_key=${API_KEY}&language=en-US`
 }
 
 export default requests;
