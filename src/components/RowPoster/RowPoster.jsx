d1e2733b12dd6398cf74cde7a4ebd4ae6147b41b
Th3Wall <mandellidavide.95@gmail.com>
2021-02-25 21:14:24 +0100

    Added margin right to each row

--- src/components/RowPoster/RowPoster.jsx
+++ src/components/RowPoster/RowPoster.jsx
@@ -1,10 +1,10 @@
 import "./rowPoster.scss"
 
 const RowPoster = ({ id, image, title, isLarge }) => {
-    const base_url = "https://image.tmdb.org/t/p/original/"
+    const base_url = "https://image.tmdb.org/t/p/original"
 
     return (
-        <div key={id} className={`Row__poster ${isLarge && "Row__poster--big"}`}>
+        <div key={id} className={`Row__poster ${isLarge ? "Row__poster--big" : ""}`}>
             <img src={`${base_url}${image}`} alt={title} loading="lazy" />
         </div>
     )
