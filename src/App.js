d2599fe560290f6502c2ff165bde8f40443265e3
Th3Wall <mandellidavide.95@gmail.com>
2021-02-28 03:05:04 +0100

    Added Header component

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import Homepage from "./pages/Homepage/Homepage"
+import Header from './components/Header/Header';
 
 function App() {
     return (
         <div className="App">
+            <Header />
             <Homepage />
         </div>
     )
---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,29 @@
+import './header.scss';
+import { LOGO_URL } from '../../requests';
+
+const Header = () => {
+    return (
+        <nav className='Header'>
+            <img className='Header__logo' src={LOGO_URL} alt="Logo"/>
+            <ul className="Header__navlinks">
+                <li className="Header__navlinks--link">
+                    Home
+                </li>
+                <li className="Header__navlinks--link">
+                    TV Series
+                </li>
+                <li className="Header__navlinks--link">
+                    Movies
+                </li>
+                <li className="Header__navlinks--link">
+                    Popular
+                </li>
+                <li className="Header__navlinks--link">
+                    My list
+                </li>
+            </ul>
+        </nav>
+    )
+}
+
+export default Header
