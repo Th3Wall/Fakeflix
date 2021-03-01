5ee24802a5e344a065fbd09e4f67d42d7efbcf23
Th3Wall <mandellidavide.95@gmail.com>
2021-03-01 01:09:52 +0100

    Added navbar transition and edited margin-top on homepage

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
+import Navbar from "./components/Navbar/Navbar"
 import Homepage from "./pages/Homepage/Homepage"
-import Header from "./components/Header/Header"
 
 function App() {
     return (
         <div className="App">
-            <Header />
+            <Navbar />
             <Homepage />
         </div>
     )
