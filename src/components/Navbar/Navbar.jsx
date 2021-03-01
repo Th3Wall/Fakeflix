2162f68679af5cd182eff59f326aa78dd49e988d
Th3Wall <mandellidavide.95@gmail.com>
2021-03-01 01:49:45 +0100

    Added viewport custom hooks and added some responsiveness styles + search toggle

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,39 +1,63 @@
 import "./navbar.scss"
-import { useState, useEffect } from 'react';
+import { useState, useEffect } from "react"
+import useViewport from "../../hooks/useViewport"
 import { LOGO_URL, PROFILE_PIC_URL } from "../../requests"
 import { FaCaretDown } from "react-icons/fa"
 import { FiSearch } from "react-icons/fi"
 
 const Navbar = () => {
-    const [fixedNav, setFixedNav] = useState(false);
+    const { width } = useViewport()
+    const [fixedNav, setFixedNav] = useState(false)
+    const [searchInput, setSearchInput] = useState(false)
 
     useEffect(() => {
-        window.addEventListener('scroll', () => {
+        window.addEventListener("scroll", () => {
             if (window.scrollY > 70) {
-                setFixedNav(true);
-            } else setFixedNav(false);
+                setFixedNav(true)
+            } else setFixedNav(false)
         })
         return () => {
-            window.removeEventListener('scroll');
+            window.removeEventListener("scroll")
         }
     }, [])
 
+    const toggleSearchInput = () => {
+        setSearchInput(!searchInput)
+    }
+
     return (
-        <nav className={`Navbar ${fixedNav ? 'Navbar__fixed' : ''}`}>
+        <nav className={`Navbar ${fixedNav ? "Navbar__fixed" : ""}`}>
             <img className="Navbar__logo" src={LOGO_URL} alt="Logo" />
-            <ul className="Navbar__primarynav Navbar__navlinks">
-                <li className="Navbar__navlinks--link">Home</li>
-                <li className="Navbar__navlinks--link">TV Series</li>
-                <li className="Navbar__navlinks--link">Movies</li>
-                <li className="Navbar__navlinks--link">Popular</li>
-                <li className="Navbar__navlinks--link">My list</li>
-            </ul>
+            {width >= 1024 ? (
+                <ul className="Navbar__primarynav Navbar__navlinks">
+                    <li className="Navbar__navlinks--link">Home</li>
+                    <li className="Navbar__navlinks--link">TV Series</li>
+                    <li className="Navbar__navlinks--link">Movies</li>
+                    <li className="Navbar__navlinks--link">Popular</li>
+                    <li className="Navbar__navlinks--link">My list</li>
+                </ul>
+            ) : (
+                <div className="Navbar__primarynav Navbar__navlinks">
+                    <span className="Navbar__navlinks--link">Show more</span>
+                    <FaCaretDown />
+                </div>
+            )}
             <div className="Navbar__secondarynav">
                 <div className="Navbar__navitem">
                     <div className="Navbar__navsearch">
-                        <input type="text" className="Navbar__navsearch--search" />
-                        <div className="Navbar__navsearch--toggler">
-                            <FiSearch size='1.5em' />
+                        <input
+                            type="text"
+                            placeholder="Search titles"
+                            className={`
+                                Navbar__navsearch--search
+                                ${ searchInput ? "Navbar__navsearch--active" : "" }
+                            `}
+                        />
+                        <div
+                            className="Navbar__navsearch--toggler"
+                            onClick={toggleSearchInput}
+                        >
+                            <FiSearch size="1.5em" />
                         </div>
                     </div>
                 </div>
---
+++ src/hooks/useViewport.jsx
@@ -0,0 +1,21 @@
+import { useState, useEffect } from "react"
+
+const useViewport = () => {
+    const [width, setWidth] = useState(window.innerWidth)
+    const [height, setHeight] = useState(window.innerHeight)
+
+    useEffect(() => {
+        const handleWindowResize = () => {
+            setWidth(window.innerWidth)
+            setHeight(window.innerHeight)
+        }
+
+        window.addEventListener("resize", handleWindowResize)
+
+        return () => window.removeEventListener("resize", handleWindowResize)
+    }, [])
+
+    return { width, height }
+}
+
+export default useViewport
