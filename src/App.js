afa0f44f25b67a70916f803e3131a9b1e3faf39c
Th3Wall <mandellidavide.95@gmail.com>
2021-03-04 00:09:53 +0100

    Added SignIn basic layout

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import { useEffect } from "react"
 import { connect } from "react-redux"
-import { Route, Switch } from "react-router-dom"
+import { Route, Switch, Redirect } from "react-router-dom"
 import Navbar from "./components/Navbar/Navbar"
 import { auth, createUserProfileDocument } from "./firebase/firebaseUtils"
 import Homepage from "./pages/Homepage/Homepage"
@@ -8,28 +8,30 @@ import { setCurrentUser } from "./redux/user/user.actions"
 import SignIn from './pages/SignIn/SignIn';
 
 const App = ({ setCurrentUser, currentUser }) => {
-    let unsubscribeFromAuth = null
+    let unsubscribeFromAuth = null;
 
     useEffect(() => {
         unsubscribeFromAuth = auth.onAuthStateChanged(async userAuth => {
             if (userAuth) {
-                const userRef = await createUserProfileDocument(userAuth)
-
+                const userRef = await createUserProfileDocument(userAuth);
                 userRef.onSnapshot(snapShot => {
                     setCurrentUser({
                         id: snapShot.id,
-                        ...snapShot.data(),
+                        ...snapShot.data()
                     })
                 })
-            } else setCurrentUser(userAuth)
+            } else setCurrentUser(userAuth);
         })
-        return () => unsubscribeFromAuth()
+        return () => unsubscribeFromAuth();
     })
 
     return (
         <div className="App">
             { currentUser && <Navbar /> }
             <Switch>
+                <Route exact path="/">
+                    <Redirect to='/login' />
+                </Route>
                 <Route path="/browse">
                     <Homepage />
                 </Route>
--- src/pages/SignIn/SignIn.jsx
+++ src/pages/SignIn/SignIn.jsx
@@ -1,10 +1,21 @@
 import './signIn.scss'
-import { SIGNIN_BGIMG_URL } from '../../requests.js';
+import { LOGO_URL, SIGNIN_BGIMG_URL } from '../../requests.js';
+import { Link } from 'react-router-dom';
 
 const SignIn = () => {
     return (
-        <div className='SignIn' style={{ backgroundImage: `url(${SIGNIN_BGIMG_URL})` }}>
+        <div className='SignIn'>
             <div className="SignIn__opacityLayer" />
+            <div
+                className="SignIn__bgLayer"
+                style={{ backgroundImage: `url(${SIGNIN_BGIMG_URL})` }}
+            />
+            <Link to='/' className='SignIn__logo'>
+                <img className="SignIn__logo--img" src={LOGO_URL} />
+            </Link>
+            <div className="SignIn__content">
+                <h2 className="SignIn__content--title">Sign In</h2>
+            </div>
         </div>
     )
 }
