bb378b14d6f6d6995480bcd3f02f1aab2e776564
Th3Wall <mandellidavide.95@gmail.com>
2021-03-21 03:01:38 +0100

    Fixed error case

--- src/components/Row/Row.jsx
+++ src/components/Row/Row.jsx
@@ -28,58 +28,60 @@ const Row = ({ selector, title, isLarge }) => {
 		<div className="Row">
 			<h3 className="Row__title">{title}</h3>
 
-			{loading && <div>Loading...</div>}
-			{error && <div>Error occurred.</div>}
-			<div className="Row__poster--wrp">
-				<div className="Row__slider--mask left" ref={navigationPrevRef}>
-					<MdChevronLeft className="Row__slider--mask-icon left" size="3em" style={{ color:'white' }} />
+			{loading && <div className='Row__not-loaded'>Loading...</div>}
+			{error && <div className='Row__not-loaded'>Error occurred.</div>}
+			{!loading && !error && (
+				<div className="Row__poster--wrp">
+					<div className="Row__slider--mask left" ref={navigationPrevRef}>
+						<MdChevronLeft className="Row__slider--mask-icon left" size="3em" style={{ color:'white' }} />
+					</div>
+					<div className="Row__slider--mask right" ref={navigationNextRef}>
+						<MdChevronRight className="Row__slider--mask-icon right" size="3em" style={{ color:'white' }} />
+					</div>
+					<Swiper
+						{...customSwiperParams}
+						navigation={{
+							prevEl: navigationPrevRef.current,
+							nextEl: navigationNextRef.current,
+						}}
+						onBeforeInit={(swiper) => {
+							swiper.params.navigation.prevEl = navigationPrevRef.current;
+							swiper.params.navigation.nextEl = navigationNextRef.current;
+						}}
+						breakpoints={{
+							1378: { slidesPerView: 6, slidesPerGroup: 6 },
+							998: { slidesPerView: 4, slidesPerGroup: 4 },
+							625: { slidesPerView: 3, slidesPerGroup: 3 },
+							330: { slidesPerView: 2, slidesPerGroup: 2 },
+							0: { slidesPerView: 1.5, slidesPerGroup: 1.5 },
+						}}
+						loopAdditionalSlides={width >= 1378 ? 5 : width >= 998 ? 3 : width >= 625 ? 2 : 2}
+						pagination
+						loop={false}
+						grabCursor={false}
+						draggable={false}
+						preventClicksPropagation={true}
+						preventClicks={true}
+						slideToClickedSlide={false}
+					>
+						{!loading &&
+							results &&
+							results.map(result => (
+								<SwiperSlide key={result.id}>
+									<RowPoster
+										item={result}
+										title={title}
+										isLarge={isLarge}
+										key={result.id}
+										isFavourite={result.isFavourite}
+										image={isLarge ? result.poster_path : result.backdrop_path}
+									/>
+								</SwiperSlide>
+							))
+						}
+					</Swiper>
 				</div>
-				<div className="Row__slider--mask right" ref={navigationNextRef}>
-					<MdChevronRight className="Row__slider--mask-icon right" size="3em" style={{ color:'white' }} />
-				</div>
-				<Swiper
-                    {...customSwiperParams}
-					navigation={{
-						prevEl: navigationPrevRef.current,
-						nextEl: navigationNextRef.current,
-					}}
-					onBeforeInit={(swiper) => {
-						swiper.params.navigation.prevEl = navigationPrevRef.current;
-						swiper.params.navigation.nextEl = navigationNextRef.current;
-					}}
-					breakpoints={{
-						1378: { slidesPerView: 6, slidesPerGroup: 6 },
-						998: { slidesPerView: 4, slidesPerGroup: 4 },
-						625: { slidesPerView: 3, slidesPerGroup: 3 },
-                        330: { slidesPerView: 2, slidesPerGroup: 2 },
-						0: { slidesPerView: 1.5, slidesPerGroup: 1.5 },
-					}}
-					loopAdditionalSlides={width >= 1378 ? 5 : width >= 998 ? 3 : width >= 625 ? 2 : 2}
-					pagination
-					loop={false}
-					grabCursor={false}
-					draggable={false}
-					preventClicksPropagation={true}
-					preventClicks={true}
-					slideToClickedSlide={false}
-				>
-					{!loading &&
-						results &&
-						results.map(result => (
-							<SwiperSlide key={result.id}>
-								<RowPoster
-									item={result}
-									title={title}
-									isLarge={isLarge}
-									key={result.id}
-									isFavourite={result.isFavourite}
-									image={isLarge ? result.poster_path : result.backdrop_path}
-								/>
-							</SwiperSlide>
-						))
-					}
-				</Swiper>
-			</div>
+			)}
 		</div>
 	);
 };
