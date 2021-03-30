6a156d1c8307ea239e5370ba4b2a22e0fcbc846a
Th3Wall <mandellidavide.95@gmail.com>
2021-03-30 03:00:55 +0200

    Added a show more label animation inline with Row titles

--- src/components/Row/Row.jsx
+++ src/components/Row/Row.jsx
@@ -5,6 +5,7 @@ import { useRef } from "react";
 import { Link, useLocation } from "react-router-dom";
 import useViewport from "../../hooks/useViewport";
 import { MdChevronLeft, MdChevronRight } from 'react-icons/md';
+import { FiChevronRight } from "react-icons/all";
 
 // Swiper
 import SwiperCore, { Navigation, Pagination } from 'swiper';
@@ -30,7 +31,8 @@ const Row = ({ selector, title, genre, isLarge }) => {
 		<div className="Row">
 			<h3 className="Row__title">
 				<Link to={`${pathname}/${genre.toLowerCase()}`}>
-					{title}
+					<span>{title}</span>
+					<span className='Row__showmore'>Show all <FiChevronRight/></span>
 				</Link>
 			</h3>
 
