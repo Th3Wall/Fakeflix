f43ee51b4def229f4ae31249c6436860d48f3d0a
Th3Wall <mandellidavide.95@gmail.com>
2021-04-01 02:15:02 +0200

    Created a generic UI animation for items details (WIP for responsiveness and modal opening)

--- src/components/RowPoster/RowPoster.jsx
+++ src/components/RowPoster/RowPoster.jsx
@@ -2,17 +2,13 @@ import "./rowPoster.scss"
 import { BASE_IMG_URL } from "../../requests"
 import { useDispatch } from 'react-redux';
 import { addToFavourites, removeFromFavourites } from "../../redux/favourites/favourites.actions";
-import { FaPlus, FaMinus } from "react-icons/fa"
+import { FaPlus, FaMinus, FaPlay, FaChevronDown } from "react-icons/fa";
 import useGenreConversion from "../../hooks/useGenreConversion";
-// import { useSelector } from 'react-redux';
-// import { selectFavouritesList } from "../../redux/favourites/favourites.selectors";
 
 const RowPoster = result => {
     const { item: { title, original_name, original_title, name, genre_ids }, image, isLarge, isFavourite } = result;
     let fallbackTitle = title || original_title || name || original_name;
     const dispatch = useDispatch();
-    // const selector = useSelector(selectFavouritesList);
-    // let isFavourite = selector.includes(result) ? true : false;
 
     const genresConverted = useGenreConversion(genre_ids);
     
@@ -25,15 +21,24 @@ const RowPoster = result => {
 
     return (
         <div className={`Row__poster ${isLarge ? "Row__poster--big" : ""}`}>
-            <button className='Button'>
-                {!isFavourite
-                    ? <FaPlus onClick={handleAdd} />
-                    : <FaMinus onClick={handleRemove} />
-                }
-            </button>
             <img src={`${BASE_IMG_URL}/${image}`} alt={fallbackTitle} />
 
             <div className="Row__poster-info">
+                <div className="Row__poster-info--iconswrp">
+                    <button className='Row__poster-info--icon icon--play'>
+                        <FaPlay />
+                    </button>
+                    <button className='Row__poster-info--icon icon--favourite'>
+                        {!isFavourite
+                            ? <FaPlus onClick={handleAdd} />
+                            : <FaMinus onClick={handleRemove} />
+                        }
+                    </button>
+
+                    <button className='Row__poster-info--icon icon--toggleModal'>
+                        <FaChevronDown />
+                    </button>
+                </div>
                 <div className="Row__poster-info--title">
                     <span>{fallbackTitle}</span>
                 </div>
