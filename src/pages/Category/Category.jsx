c294f060fed897cfdb63971156d1d25a92aa7567
Th3Wall <mandellidavide.95@gmail.com>
2021-04-15 23:23:48 +0200

    Displayed movie categories

--- src/pages/Category/Category.jsx
+++ src/pages/Category/Category.jsx
@@ -3,6 +3,7 @@ import { useState } from "react";
 import { useRetrieveCategory } from "../../hooks/useRetrieveCategory";
 import { useSelector } from "react-redux";
 import { useParams } from "react-router-dom";
+import SearchResults from "../../components/SearchResults/SearchResults";
 
 const Category = ({match}) => {
     const [page, setPage] = useState(1);
@@ -10,21 +11,33 @@ const Category = ({match}) => {
     const slicedUrl = url.split("/");
     const { categoryName } = useParams();
     const categoryData = useRetrieveCategory(slicedUrl[1], categoryName, page);
-
-    const selectorThatReturnsUndefined = () => undefined
-    const selector = categoryData ? categoryData.selector : selectorThatReturnsUndefined
-    const data = useSelector(selector)
-    console.log("data => ", data);
+    const preventUndefinedSelector = () => undefined;
+    const selector = categoryData ? categoryData.selector : preventUndefinedSelector;
+    const selectedGenre = useSelector(selector);
 
     return (
-        <>
+        <div className="Category">
             {categoryData && (
-                <div className="Category">
+                <>
                     <h2 className="Category__title">{categoryData.title}</h2>
                     <button onClick={()=> setPage(page+1)}>Change page</button>
-                </div>
+
+                    <div className="Category__wrp">
+                        {!selectedGenre.loading && !selectedGenre.error && selectedGenre.data && selectedGenre.data.length > 0
+                            && selectedGenre.data.map(result => (
+                                <SearchResults
+                                    key={result.id}
+                                    item={result}
+                                    {...result}
+                                />
+                            ))
+                        }
+                        {selectedGenre.loading && <div className='Category__subtitle'>Loading...</div>}
+                        {selectedGenre.error && <div className='Category__subtitle'>Error occurred.</div>}
+                    </div>
+                </>
             )}
-        </>
+        </div>
     )
 }
 
