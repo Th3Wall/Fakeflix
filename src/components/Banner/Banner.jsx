d35ec4a3b7599f2b4ad03d2af42d779143fb1812
Th3Wall <mandellidavide.95@gmail.com>
2021-04-28 02:10:27 +0200

    WIP Banner refactor

--- src/components/Banner/Banner.jsx
+++ src/components/Banner/Banner.jsx
@@ -1,39 +1,31 @@
 import "./banner.scss";
-import requests, { BASE_IMG_URL } from "../../requests";
-import axios from "../../axiosInstance";
-import { useState, useEffect } from "react";
+import { BASE_IMG_URL } from "../../requests";
 import { FaPlay } from "react-icons/fa";
 import { BiInfoCircle } from "react-icons/bi";
 import { randomize, truncate } from "../../utils";
 import { Link } from "react-router-dom";
-
-const { fetchNetflixOriginals, discoverMovies, discoverSeries } = requests;
+import { selectNetflixMovies } from "../../redux/movies/movies.selectors";
+import { selectNetflixSeries } from "../../redux/series/series.selectors";
+import { useSelector } from "react-redux";
 
 const Banner = ({ type }) => {
-	const [banner, setBanner] = useState([]);
-	let fetchReq = null;
-
+	let selector = null;
 	switch (type) {
 		case "movies":
-			fetchReq = discoverMovies;
+			selector = selectNetflixMovies;
 			break;
 		case "series":
-			fetchReq = discoverSeries;
+			selector = selectNetflixSeries;
 			break;
 		default:
-			fetchReq = fetchNetflixOriginals;
+			selector = selectNetflixMovies;
 			break;
 	}
 
-	useEffect(() => {
-		const fetchBannerData = async () => {
-			const res = await axios.get(fetchReq);
-			const { data: { results } } = res;
-			setBanner(results[randomize(results)]);
-		};
-
-		fetchBannerData();
-	}, [fetchReq]);
+	const myData = useSelector(selector);
+	const { loading, error, data: results } = myData;
+	const finalData = results[randomize(results)];
+	let fallbackTitle = finalData?.title || finalData?.name || finalData?.original_name;
 
 	const handlePlayAnimation = event => {
 		event.stopPropagation();
@@ -41,16 +33,19 @@ const Banner = ({ type }) => {
 
 	return (
 		<>
-			<header
-				className="Banner"
-				style={{
-					backgroundImage: `url(${BASE_IMG_URL}/${banner?.backdrop_path})`,
-				}}
-			>
-				{banner && (
-					<>
+			{loading && <div className='Banner'>Loading...</div>}
+			{error && <div className='Row__not-loaded'>Error occurred.</div>}
+
+			{!loading &&
+				finalData && (
+					<header
+						className="Banner"
+						style={{
+							backgroundImage: `url(${BASE_IMG_URL}/${finalData.backdrop_path})`,
+						}}
+					>
 						<div className="Banner__content">
-							<h1 className="Banner__content--title">{banner.title || banner.name || banner.original_name}</h1>
+							<h1 className="Banner__content--title">{fallbackTitle}</h1>
 							<div className="Banner__buttons">
 								<Link
 									className="Banner__button"
@@ -65,13 +60,13 @@ const Banner = ({ type }) => {
 									<span>More info</span>
 								</button>
 							</div>
-							<p className="Banner__content--description">{truncate(banner.overview, 280)}</p>
+							<p className="Banner__content--description">{truncate(finalData.overview, 280)}</p>
 						</div>
 						<div className="Banner__panel" />
 						<div className="Banner__bottom-shadow" />
-					</>
-				)}
-			</header>
+					</header>
+				)
+			}
 		</>
 	);
 };
